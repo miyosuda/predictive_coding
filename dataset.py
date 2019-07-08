5a220fdd2957ba11eb2420d36f7fd1d8e6a2b365
kosuke miyoshi <miyoshi@narr.jp>
2019-07-08 14:19:42 +0900

    fix dataset bug

--- dataset.py
+++ dataset.py
@@ -6,17 +6,30 @@ import cv2
 
 class Dataset:
     def __init__(self):
-        file_path = "./data/IMAGES_RAW.mat"
-        matdata = io.loadmat(file_path)
+        self.load_images()
+        #self.load_matlab()
 
-        images = matdata['IMAGESr'].astype(np.float32)
-        # Change image order
-        images = np.array([images[:,:,i] for i in range(images.shape[2])])
+    def load_images(self):
+        images = np.empty([5,408,512])
+        
+        for i in range(len(images)):
+            image = cv2.imread("data/image{}.png".format(i))
+            #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255.0
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32)
+            images[i] = image
 
+        self.load_sub(images)
+
+
+    def load_sub(self, images):
+        self.images = images
+        
         filtered_images = []
         for image in images:
             filtered_image = self.apply_DoG_filter(image)
             filtered_images.append(filtered_image)
+
+        self.filtered_images = filtered_images
 
         w = images.shape[2]
         h = images.shape[1]
@@ -25,7 +38,8 @@ class Dataset:
         size_h = h // 16
 
         patches = np.empty((size_h * size_w * len(images), 16, 26), dtype=np.float32)
-        for filtered_image in filtered_images:
+
+        for image_index, filtered_image in enumerate(filtered_images):
             for j in range(size_h):
                 y = 16 * j
                 for i in range(size_w):
@@ -33,9 +47,20 @@ class Dataset:
                     patch = filtered_image[y:y+16, x:x+26]
                     # (16, 26)
                     # print(patch.shape)
-                    patches[j*size_w + i] = patch
-
+                    index = size_w*size_h*image_index + j*size_w + i
+                    patches[index] = patch
         self.patches = patches
+            
+
+    def load_matlab(self):
+        file_path = "./data/IMAGES_RAW.mat"
+        matdata = io.loadmat(file_path)
+
+        images = matdata['IMAGESr'].astype(np.float32)
+        # Change image order
+        images = np.array([images[:,:,i] for i in range(images.shape[2])])
+        self.load_sub(images)
+
 
     def apply_DoG_filter(self, gray, ksize=(5,5), sigma1=1.3, sigma2=2.6):
         g1 = cv2.GaussianBlur(gray, ksize, sigma1)
--- dataset_test.py
+++ dataset_test.py
@@ -7,7 +7,8 @@ from dataset import Dataset
 class DatasetTest(unittest.TestCase):
     def test_init(self):
         dataset = Dataset()
-        self.assertEqual(dataset.patches.shape, (6080,16,26))
+        #self.assertEqual(dataset.patches.shape, (6080,16,26))
+        self.assertEqual(dataset.patches.shape, (2375,16,26))
 
 
 if __name__ == '__main__':
