4943ec97490596f5afc9e4f39c399d143379b06c
kosuke miyoshi <miyoshi@narr.jp>
2019-07-09 12:22:55 +0900

    refactor dataset class

--- dataset.py
+++ dataset.py
@@ -5,26 +5,19 @@ import cv2
 
 
 class Dataset:
-    def __init__(self, normalize=False):
-        self.load_images(normalize)
-        #self.load_matlab()
+    def __init__(self, scale=10.0):
+        self.load_images(scale)
 
-    def load_images(self, normalize):
+    def load_images(self, scale):
         images = np.empty([5,408,512])
-        #images = np.empty([5,512,512])
         
         for i in range(len(images)):
             image = cv2.imread("data/images_rao/image{}.png".format(i))
-            #image = cv2.imread("data/images_aa/image{}.png".format(i))
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32)
-            if normalize:
-                image = image / 255.0
             images[i] = image
+        self.load_sub(images, scale)
 
-        self.load_sub(images)
-
-
-    def load_sub(self, images):
+    def load_sub(self, images, scale):
         self.images = images
         
         filtered_images = []
@@ -52,9 +45,24 @@ class Dataset:
                     # print(patch.shape)
                     index = size_w*size_h*image_index + j*size_w + i
                     patches[index] = patch
-        self.patches = patches
-            
 
+        patches = patches * scale
+        self.patches = patches
+
+    def get_image(self, patch_index, image_index):
+        """
+        Arguments:
+          patch_index: index of the patch
+          image_index: 0,1,2
+
+        Returns:
+          nd-array: (256,)
+        """
+        patch = self.patches[patch_index]
+        x = 5 * image_index
+        return patch[:, x:x+16].reshape([-1])
+        
+    """
     def load_matlab(self):
         file_path = "./data/IMAGES_RAW.mat"
         matdata = io.loadmat(file_path)
@@ -63,7 +71,7 @@ class Dataset:
         # Change image order
         images = np.array([images[:,:,i] for i in range(images.shape[2])])
         self.load_sub(images)
-
+    """
 
     def apply_DoG_filter(self, gray, ksize=(5,5), sigma1=1.3, sigma2=2.6):
         g1 = cv2.GaussianBlur(gray, ksize, sigma1)
--- dataset_test.py
+++ dataset_test.py
@@ -6,9 +6,11 @@ from dataset import Dataset
 
 class DatasetTest(unittest.TestCase):
     def test_init(self):
-        dataset = Dataset()
-        #self.assertEqual(dataset.patches.shape, (6080,16,26))
+        dataset = Dataset(scale=10.0)
         self.assertEqual(dataset.patches.shape, (2375,16,26))
+
+        image = dataset.get_image(0, 0)
+        self.assertEqual(image.shape, (256,))
 
 
 if __name__ == '__main__':
