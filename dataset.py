acc473b81f080941a33aa4aaa1f3252e6b04179e
kosuke miyoshi <miyoshi@narr.jp>
2019-07-09 12:59:13 +0900

    apply gaussian mask for image input

--- dataset.py
+++ dataset.py
@@ -7,15 +7,36 @@ import cv2
 class Dataset:
     def __init__(self, scale=10.0):
         self.load_images(scale)
+        self.mask = self.create_gauss_mask()
 
     def load_images(self, scale):
         images = np.empty([5,408,512])
+        #images = np.empty([5,512,512])
         
         for i in range(len(images)):
             image = cv2.imread("data/images_rao/image{}.png".format(i))
+            #image = cv2.imread("data/images_aa/image{}.png".format(i))
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32)
             images[i] = image
         self.load_sub(images, scale)
+
+    def create_gauss_mask(self, width=16, sigma=0.4): 
+        rates = [0.0] * (width * width)
+        hw = width // 2
+        for i in range(width):
+            x = (i - hw) / float(hw)
+            for j in range(width):
+                y = (j - hw) / float(hw)
+                r = np.sqrt(x*x + y*y)
+                rates[j*width + i] = self.gauss(r, sigma=sigma)
+        rates = np.array(rates)
+        # Normalize
+        rates = rates / np.max(rates)
+        return rates
+
+    def gauss(self, x, sigma):
+        sigma_sq = sigma * sigma
+        return 1.0 / np.sqrt(2.0 * np.pi * sigma_sq) * np.exp(-x*x/(2 * sigma_sq))
 
     def load_sub(self, images, scale):
         self.images = images
@@ -60,18 +81,16 @@ class Dataset:
         """
         patch = self.patches[patch_index]
         x = 5 * image_index
-        return patch[:, x:x+16].reshape([-1])
-        
-    """
-    def load_matlab(self):
+        return patch[:, x:x+16].reshape([-1]) * self.mask
+
+    def load_matlab(self, scale):
         file_path = "./data/IMAGES_RAW.mat"
         matdata = io.loadmat(file_path)
 
         images = matdata['IMAGESr'].astype(np.float32)
         # Change image order
         images = np.array([images[:,:,i] for i in range(images.shape[2])])
-        self.load_sub(images)
-    """
+        self.load_sub(images, scale)
 
     def apply_DoG_filter(self, gray, ksize=(5,5), sigma1=1.3, sigma2=2.6):
         g1 = cv2.GaussianBlur(gray, ksize, sigma1)
